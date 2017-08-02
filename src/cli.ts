b5fd9baac96edfd3a67a5e52e6f868f5801da77f
Tylor Steinberger <tlsteinberger167@gmail.com>
2017-08-02 19:31:48 -0400

    fix(cli): parse arguments correctly

--- src/cli.ts
+++ src/cli.ts
@@ -7,8 +7,8 @@ import { compile } from './compile'
 
 const args: any = yargs
   .usage(`\n$0 path/to/file\n` + `  -out -o :: where to output your compiled file`)
-  .option('out', { alias: 'o', requiresArg: true })
-  .showHelpOnFail(true)
+  .option('out', { alias: 'o' })
+  .showHelpOnFail(true).argv
 
 if (args.help) {
   args.showHelp()
